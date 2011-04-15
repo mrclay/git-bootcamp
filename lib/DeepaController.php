7122ee580d3a81d07443c83a8042c31cc506cf03
Deepa Gupta <d.gupta@ufl.edu>
2011-04-15 16:42:43 -0400

    Added Deepa Controller

---
+++ lib/DeepaController.php
@@ -0,0 +1,20 @@
+<?php
+
+class DeepaController extends IndexController {
+
+    public function helloAction() {
+        // Customize your greeting
+        //
+        // Warning: This will be visible on Github for awhile
+        
+        ?>
+
+<h1>Hy, my name is Deepa</h1>
+<p>I like to do things!</p>
+
+        <?php
+
+        // In a real app, we'd pull this from a DB and render it using a view script,
+        // but for this excerse, this will do.
+    }
+}
