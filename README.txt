194c061e105f4048ace500a1b91787d1e235bf91
Steve Clay <steve@mrclay.org>
2011-04-15 11:00:15 -0400

    better docs

--- README.txt
+++ README.txt
@@ -2,6 +2,8 @@ Welcome to Git Bootcamp
 
 Campers, add your name to the list below:
 
+(Remember: this will be visible on Github for awhile)
+
 * Steve
 *
 
--- lib/SteveController.php
+++ lib/SteveController.php
@@ -5,13 +5,16 @@ class SteveController extends IndexController {
     public function helloAction() {
         // Customize your greeting
         //
-        // In a real app, we'd pull this from a DB and render it using a view script,
-        // but for this excerse, this will do.
+        // Warning: This will be visible on Github for awhile
+        
         ?>
 
 <h1>Hy, my name is Steve!</h1>
 <p>I like to do things!</p>
 
         <?php
+
+        // In a real app, we'd pull this from a DB and render it using a view script,
+        // but for this excerse, this will do.
     }
 }
